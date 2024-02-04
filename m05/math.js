a7c67a9ab786db8c5b78ba6ec2a9cd12d49e65d2
caitba18 <155859782+caitba18@users.noreply.github.com>
2024-02-03 17:46:33 -0800

    Create math.js

---
+++ m05/math.js
@@ -0,0 +1,14 @@
+var table = prompt("Please enter a number from 1-10");             // user enters number that will be in the multiplication table
+var operator = 'multiplication'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '';              // Message
+
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
