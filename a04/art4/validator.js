2f402813181951d55fb5f73ce92f3f934829021f
caitba18 <155859782+caitba18@users.noreply.github.com>
2024-03-06 11:45:31 -0800

    Update validator.js

--- a04/art4/validator.js
+++ a04/art4/validator.js
@@ -1,8 +1,6 @@
 function isValid() {
-    if (firstName() //&&
-       // lastName()
-    )
-    return true;
+    if (firstName() && lastName()) //&& email() && phone() && username() && password() && address() && city() && zipcode())
+        return true;
     else
         document.getElementById("submiterror").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
         event.preventDefault();
@@ -23,7 +21,7 @@ function firstName(){
         errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
         console.log("First name invalid — length")
         } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
-            errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+            errorMessages += "<p>Invalid caracter in first name (accepts only A-Z, a-z, and ,.'-)</p>";
             console.log("First name invalid — bad characters")
         } else {
                 validFirstname = true;
@@ -31,8 +29,25 @@ function firstName(){
         };
 
     //4) Send error message to HTML
-    document.getElementById("fname").innerHTML = errorMessages;
+    document.getElementById("FirstName").innerHTML = errorMessages;
 
     //5) return status of each field
     return (validFirstname);
 };
+
+function lastName() {
+    var validLastName = false; 
+    var lastName = document.getElementById("LastName").value;
+    var errorMessages = ""; 
+
+    if (lastName==="null" || lastName==="" || lastName.length > 50) {
+        errorMessages += "<p>The last name is required and cannot be greater than 50 characters</p>";
+        console.log("Last name invalid — length")
+        } else if (lastName.match("^[a-zA-Z ,.'-]+$")===null) {
+            errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+            console.log("Last name invalid — bad characters")
+        } else {
+                validLastName = true;
+                console.log("Last name valid")
+        };
+}
