64fd815b07bb60e6acd3f1c7ddeb0fb582bbe55a
caitba18 <155859782+caitba18@users.noreply.github.com>
2024-03-12 23:15:50 -0700

    Update slides.js

--- a05/slides.js
+++ a05/slides.js
@@ -1,33 +1,30 @@
+var autoswitch;
 var slideIndex = 1;
-var autoswitch = null;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
-  clearTimeout(autoswitch);
-  console.log("ajsdsd");
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
-  clearTimeout(autoswitch);
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  
-  if (n===undefined){n = slideIndex + 1;}
-  if (n > slides.length) {slideIndex = 1; }
-  if (n < 1) {slideIndex = slides.length; }
+ 
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  
-  slides[slideIndex-1].style.display = "block";
-  autoswitch = setTimeout(showSlides, 2000);
-  
+  slides[slideIndex - 1].style.display = "block";
+  clearTimeout(autoswitch);
+  autoswitch = setTimeout(plusSlides, 2000, 1);
 }
   
